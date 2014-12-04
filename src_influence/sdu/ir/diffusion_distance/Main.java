b6f6339bdd4a539c21cd13415a5aed1783a1fb51
dongyaQin <1058584193@qq.com>
2014-12-04 17:03:59 +0800

    create matlab input of diffusion distance

--- src_influence/sdu/ir/diffusion_distance/Main.java
+++ src_influence/sdu/ir/diffusion_distance/Main.java
@@ -19,14 +19,14 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String dataName = "ca_HepPh1";
+		String dataName = "EmailEuAll";
 		double begin = System.currentTimeMillis();
-		 String filePath = "ccir2014\\"+dataName+".txt";
+		 String filePath = "E:\\dataset\\ccir2014\\"+dataName+".txt";
 		 ReadGraph rd = new ReadGraph();
-		 Graph gh = rd.readTxtFile2Graph(filePath, "Adjacentlistwithoutweight",2," ");
+		 Graph gh = rd.readTxtFile2Graph(filePath, "Adjacentlistwithoutweight",2,"\t");
 //		 Print.print(gh);
 //		 Util.block();
-		 int executions = 1000;//icm中模拟次数
+		 int executions = 2000;//icm中模拟次数
 		 int set = 50;//集合大小
 		 double[] p = new double[]{0.5,0.3,0.1};
 		 IndependentCascadeModel_RecordDistance icm = new IndependentCascadeModel_RecordDistance(executions,0.05);
@@ -34,11 +34,11 @@ public class Main {
 		 DiffusionModel dm = icm;//选择要使用的传播模型
 		 Set set1 = new HashSet();
 //		 int i = 875;
-		 double pp = 0.01;
-		 while(pp < 0.2){
+		 double pp = 0.05;
+//		 while(pp < 0.2){
 			 icm.setDiffusionProbability(pp);
 			 StringBuffer sb = new StringBuffer();
-			 for (int i = 0; i < 12007; i++) {
+			 for (int i = 20000; i < 30000; i++) {
 				 set1.add(i);
 				 Node node = icm.diffusion1(gh, set1);
 				 System.out.println(i+"-->"+node.num_infect+"==>"+node.distance);
@@ -49,7 +49,7 @@ public class Main {
 			System.out.println("totalTime ===>"+(System.currentTimeMillis()-begin)/1000f+"seconds");	
 			Util.block("输入。。。");
 			pp = pp + 0.02;
-		 }
+//		 }
 		 
 		 System.out.println("totalTime ===>"+(System.currentTimeMillis()-begin)/1000f+"seconds");
 	
